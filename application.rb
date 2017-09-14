996274056e0e2aaf209e1edc509759a34bae7f72
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-14 02:53:06 +0000

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Melissa"
+    # resp.write "Hello, I'm Melissa and I'm proud of myself today.
+    # Rather than thinking negative thoughts or worrying,
+    # I will think postivelyand focus on what I've achieved for sure today :D"
     resp.finish
   end
 
 end
-
--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
+#config.ru
 require_relative "./application.rb"
 
 run Application.new
+ #We need to actually set up an HTTP web server that will receive the HTTP request
+# so that it can then, send it through the above call method, and then serve the response to the browser
